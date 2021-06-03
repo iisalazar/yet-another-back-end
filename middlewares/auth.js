631aac51cf13ad826972f6a91988e7c265d17098
Ian Salazar <iisalazar@up.edu.ph>
2021-06-03 17:37:03 +0800

    refactor: changed from checking headers to checking cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,41 +1,46 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
   // if (!req.cookies || !req.cookies.authToken || !req.headers.authorization){
-  if ( !req.headers.authorization) {
-    res.status(403)
+  if (!req.cookies || !req.cookies.authToken) {
+    res.status(403);
     return res.json({
       success: false,
-      msg: "Unauthorized"
-    })
+      msg: "Unauthorized",
+      isLoggedIn: false,
+    });
   }
 
-  const authHeader = req.headers.authorization;
+  const authToken = req.cookies.authToken;
 
-  if(authHeader){
-    const token = authHeader.split(' ')[1];
+  if (authToken) {
+    const token = authToken;
     try {
       // payload is id
-     const payload = await jwt.verify(token, process.env.SECRET_KEY || 'secret123'); 
-     const user = await User.findById(payload);
-     if(!user){
-       res.status(404);
-       return res.json({
-         msg: `Error in middleware, can't find user with id ${payload}`,
-         success: false
-       })
-     }
-     next();
-    } catch(err){
+      const payload = await jwt.verify(
+        token,
+        process.env.SECRET_KEY || "secret123"
+      );
+      const user = await User.findById(payload);
+      if (!user) {
+        res.status(404);
+        return res.json({
+          msg: `Error in middleware, can't find user with id ${payload}`,
+          success: false,
+        });
+      }
+      next();
+    } catch (err) {
       console.log(err);
-      res.status(400)
+      res.status(400);
       return res.json({
         success: false,
-        msg: "Invalid token..."
-      })
+        msg: "Invalid token...",
+      });
     }
   }
 
   next();
-}
+};
+
