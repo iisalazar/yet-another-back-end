939aa93185a702c5e903a91270e08ab0408a14aa
Ian Salazar <iisalazar@up.edu.ph>
2021-06-04 13:52:24 +0800

    feat: added auth middleware to post endpoints

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,45 +2,50 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-  // if (!req.cookies || !req.cookies.authToken || !req.headers.authorization){
-  if (!req.cookies || !req.cookies.authToken) {
-    res.status(403);
-    return res.json({
-      success: false,
-      msg: "Unauthorized",
-      isLoggedIn: false,
-    });
-  }
+	// if (!req.cookies || !req.cookies.authToken || !req.headers.authorization){
+	console.log("Middleware running...");
+	if (!req.cookies || !req.cookies.authToken) {
+		res.status(403);
+		return res.json({
+			success: false,
+			msg: "Unauthorized",
+			isLoggedIn: false,
+		});
+	}
 
-  const authToken = req.cookies.authToken;
+	console.log("Etits?");
 
-  if (authToken) {
-    const token = authToken;
-    try {
-      // payload is id
-      const payload = await jwt.verify(
-        token,
-        process.env.SECRET_KEY || "secret123"
-      );
-      const user = await User.findById(payload);
-      if (!user) {
-        res.status(404);
-        return res.json({
-          msg: `Error in middleware, can't find user with id ${payload}`,
-          success: false,
-        });
-      }
-      next();
-    } catch (err) {
-      console.log(err);
-      res.status(400);
-      return res.json({
-        success: false,
-        msg: "Invalid token...",
-      });
-    }
-  }
+	const authToken = req.cookies.authToken;
 
-  next();
+	if (authToken) {
+		const token = authToken;
+		try {
+			// payload is id
+			const payload = await jwt.verify(
+				token,
+				process.env.SECRET_KEY || "secret123"
+			);
+			const user = await User.findById(payload);
+			if (!user) {
+				res.status(404);
+				return res.json({
+					msg: `Error in middleware, can't find user with id ${payload}`,
+					success: false,
+				});
+			}
+			return next();
+		} catch (err) {
+			console.log(err);
+			res.status(400);
+			return res.json({
+				success: false,
+				msg: "Invalid token...",
+			});
+		}
+	}
+	res.status(403);
+	return res.json({
+		success: false,
+		msg: "Auth token in cookies required",
+	});
 };
-
--- routes.js
+++ routes.js
@@ -9,11 +9,15 @@ module.exports = (app) => {
 	app.get("/users/checkAuth", controllers.userController.checkAuth);
 
 	// for post related requests
-	app.get("/posts", controllers.postController.list);
-	app.get("/posts/:id", controllers.postController.get);
-	app.patch("/posts/:id", controllers.postController.update);
-	app.delete("/posts/:id", controllers.postController.delete);
-	app.post("/posts/create", controllers.postController.create);
+	app.get("/posts", [authMiddleware], controllers.postController.list);
+	app.get("/posts/:id", [authMiddleware], controllers.postController.get);
+	app.patch("/posts/:id", [authMiddleware], controllers.postController.update);
+	app.delete("/posts/:id", [authMiddleware], controllers.postController.delete);
+	app.post(
+		"/posts/create",
+		[authMiddleware],
+		controllers.postController.create
+	);
 
 	app.get("/protected", [authMiddleware], (req, res) => {
 		res.send("Authenticated, cool");
