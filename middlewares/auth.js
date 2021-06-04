dfbf18fff0c7ceeb013672d65bc7534dab083c76
Ian Salazar <iisalazar@up.edu.ph>
2021-06-04 19:51:11 +0800

    refactor: removed unnecessary comments

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,51 +2,49 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 module.exports = async (req, res, next) => {
-  // if (!req.cookies || !req.cookies.authToken || !req.headers.authorization){
-  console.log("Middleware running...");
-  if (!req.cookies || !req.cookies.authToken) {
-    res.status(403);
-    return res.json({
-      success: false,
-      msg: "Unauthorized",
-      isLoggedIn: false,
-    });
-  }
+	// if (!req.cookies || !req.cookies.authToken || !req.headers.authorization){
+	console.log("Middleware running...");
+	if (!req.cookies || !req.cookies.authToken) {
+		res.status(403);
+		return res.json({
+			success: false,
+			msg: "Unauthorized",
+			isLoggedIn: false,
+		});
+	}
 
-  console.log("Etits?");
+	const authToken = req.cookies.authToken;
 
-  const authToken = req.cookies.authToken;
-
-  if (authToken) {
-    const token = authToken;
-    try {
-      // payload is id
-      const payload = await jwt.verify(
-        token,
-        process.env.SECRET_KEY || "secret123"
-      );
-      const user = await User.findById(payload);
-      if (!user) {
-        res.status(404);
-        return res.json({
-          msg: `Error in middleware, can't find user with id ${payload}`,
-          success: false,
-        });
-      }
-      req.user = user;
-      return next();
-    } catch (err) {
-      console.log(err);
-      res.status(400);
-      return res.json({
-        success: false,
-        msg: "Invalid token...",
-      });
-    }
-  }
-  res.status(403);
-  return res.json({
-    success: false,
-    msg: "Auth token in cookies required",
-  });
+	if (authToken) {
+		const token = authToken;
+		try {
+			// payload is id
+			const payload = await jwt.verify(
+				token,
+				process.env.SECRET_KEY || "secret123"
+			);
+			const user = await User.findById(payload);
+			if (!user) {
+				res.status(404);
+				return res.json({
+					msg: `Error in middleware, can't find user with id ${payload}`,
+					success: false,
+				});
+			}
+			req.user = user;
+			return next();
+		} catch (err) {
+			console.log(err);
+			res.status(400);
+			return res.json({
+				success: false,
+				msg: "Invalid token...",
+			});
+		}
+	}
+	res.status(403);
+	return res.json({
+		success: false,
+		msg: "Auth token in cookies required",
+	});
 };
